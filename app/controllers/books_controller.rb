24abaac496fac4db8b24a7f02dea9314fab1174c
EC2 Default User <149554348+AoKeito@users.noreply.github.com>
2024-01-04 16:04:04 +0000

    演習問題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,9 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
     @user = @book.user
+    @post_comment = PostComment.new
   end
 
   def index
